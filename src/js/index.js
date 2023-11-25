a6a209994f5b8a911bf4407f20d128575d1d63e6
VorobeiIvan <0675361905vi@gmail.com>
2023-11-25 15:56:57 +0200

    changes according to the mentor's comments

--- src/js/index.js
+++ src/js/index.js
@@ -12,80 +12,102 @@ const el = {
 let lightbox = new SimpleLightbox('.gallery-link', {
   captions: true,
 });
-
+let searchQuery = '';
 let currentPage = 1;
 
-el.searchForm.addEventListener('submit', async (event) => {
+el.searchForm.addEventListener('submit', async event => {
   event.preventDefault();
 
-  const searchQuery = el.searchQueryInput.value.trim();
-  if (searchQuery) {
-    try {
-      const { hits, totalHits } = await performSearch(searchQuery, currentPage);
-
-      if (totalHits === 0) {
-        displayNoResultsMessage();
-        return;
-      }
-
-      Notiflix.Report.success('Search Results', `Hooray! We found ${totalHits} images.`, 'OK');
-
-      displayImages(hits, totalHits);
-      // el.searchQueryInput.value = '';
-
-      if (hits.length >= 40) {
-        el.loadMoreBtn.style.display = 'block';
-      } else {
-        el.loadMoreBtn.style.display = 'none';
-      }
-    } catch (error) {
-      console.error('Search error:', error);
-
-      Notiflix.Report.failure('Search Error', 'Sorry, there was an error retrieving the images. Please try again later.', 'OK');
-    }
-  } else {
-    Notiflix.Report.failure("Please enter a request", 'OK');
+  searchQuery = el.searchQueryInput.value.trim();
+  if (!searchQuery) {
+    Notiflix.Report.failure('Please enter a request', 'OK');
     el.searchQueryInput.value = '';
+    return;
+  }
+
+  try {
+    const { hits, totalHits } = await performSearch(searchQuery, currentPage);
+
+    if (totalHits === 0) {
+      displayNoResultsMessage();
+      return;
+    }
+
+    if (totalHits <= 40) {
+      Notiflix.Report.info(
+        'Search Results',
+        `Hooray! We found ${totalHits} images. Sorry, but that's all I could find. You have reached the end of your search.`,
+        'OK'
+      );
+    } else {
+      Notiflix.Report.success(
+        'Search Results',
+        `Hooray! We found ${totalHits} images.`,
+        'OK'
+      );
+    }
+
+    displayImages(hits, totalHits);
+    checkingTheNumberOfCards(hits);
+  } catch (error) {
+    console.error('Search error:', error);
+
+    Notiflix.Report.failure(
+      'Search Error',
+      'Sorry, there was an error retrieving the images. Please try again later.',
+      'OK'
+    );
   }
 });
 
-el.loadMoreBtn.addEventListener('click', async (event) => {
+el.loadMoreBtn.addEventListener('click', async event => {
   currentPage += 1;
-  addNextPageImages();
+  try {
+    const { hits } = await performSearch(searchQuery, currentPage);
+    checkingTheNumberOfCards(hits);
+    el.gallery.insertAdjacentHTML('beforeend', createMarkup(hits));
+  } catch (error) {
+    console.error('Load more error:', error);
+  }
 });
 
-function addNextPageImages() {
-  performSearch(el.searchQueryInput.value, currentPage)
-    .then(({ hits }) => {
-      el.gallery.insertAdjacentHTML('beforeend', createMarkup(hits));
-    })
-    .catch((error) => {
-      console.error('Load more error:', error);
-    });
-}
-
-function displayImages(hits, totalHits) {
-  const markup = createMarkup(hits);
-  el.gallery.innerHTML = markup;
+function displayImages(hits) {
+  el.gallery.innerHTML = createMarkup(hits);
   initializeLightbox();
-
-  if (totalHits / hits.length === currentPage) {
+}
+function checkingTheNumberOfCards(hits) {
+  if (hits.length >= 40) {
     el.loadMoreBtn.style.display = 'block';
   } else {
     el.loadMoreBtn.style.display = 'none';
-    Notiflix.Report.info('End of Results', "We're sorry, but you've reached the end of search results.", 'OK');
+    Notiflix.Report.info(
+      'End of Results',
+      "We're sorry, but you've reached the end of search results.",
+      'OK'
+    );
   }
 }
-
 function displayNoResultsMessage() {
-  el.gallery.innerHTML = '<p>No results found. Please try another search.</p>';
-  el.loadMoreBtn.style.display = 'none';
+  Notiflix.Report.failure(
+    'No results found',
+    'Please try another search',
+    'OK'
+  );
 }
 
 function createMarkup(galleryItems) {
   return galleryItems
-    .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
-      return `
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `
         <div class="photo-card">
           <a class="gallery-link" href="${largeImageURL}" title="${tags}" >
             <img src="${webformatURL}" alt="${tags}" width="300" loading="lazy">
@@ -102,7 +124,8 @@ function createMarkup(galleryItems) {
           </div>
         </div>
       `;
-    })
+      }
+    )
     .join('');
 }
 
